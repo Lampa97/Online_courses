ecd46f41aff5d225d18267a35d23bf2d40d355e6
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-02-17 19:43:28 +0300

    created User, Course and Lesson models

--- education/models.py
+++ education/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=150, verbose_name="Title")
+    preview = models.ImageField(upload_to="courses/", default="courses/default_course.png", verbose_name="Preview")
+    description = models.TextField(verbose_name="Description")
+
+    class Meta:
+        verbose_name = "Course"
+        verbose_name_plural = "Courses"
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=150, verbose_name="Title")
+    description = models.TextField(verbose_name="Description")
+    preview = models.ImageField(upload_to="lessons/", default="lessons/default_lesson.png", verbose_name="Preview")
+    video_link = models.URLField(verbose_name="Video link")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="lessons", verbose_name="Course")
+
+    class Meta:
+        verbose_name = "Lesson"
+        verbose_name_plural = "Lessons"
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True, verbose_name="Phone_number")
+    city = models.CharField(max_length=50, blank=True, null=True, verbose_name="City")
+    avatar = models.ImageField(
+        upload_to="avatars/", default="avatars/default_avatar.jpg", blank=True, null=True, verbose_name="Avatar"
+    )
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+        ordering = [
+            "email",
+        ]
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
