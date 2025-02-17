5c71226a28ab994f4f10091380ad5efd177894c4
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-02-17 19:58:42 +0300

    added serializer and Viewset based CRUD for Course model

--- education/views.py
+++ education/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from .models import Course, Lesson
+from django.shortcuts import get_object_or_404
+from .serializers import CourseSerializer, LessonSerializer
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+
+class CourseViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        queryset = Course.objects.all()
+        serializer = CourseSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+    def retrieve(self, request, pk=None):
+        queryset = Course.objects.all()
+        course = get_object_or_404(queryset, pk=pk)
+        serializer = CourseSerializer(course)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        queryset = Course.objects.all()
+        course = get_object_or_404(queryset, pk=pk)
+        serializer = CourseSerializer(course, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = CourseSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        queryset = Course.objects.all()
+        course = get_object_or_404(queryset, pk=pk)
+        course.delete()
+        return Response({"message": "Course deleted successfully"})
--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from .models import User
+from django.shortcuts import get_object_or_404
+from .serializers import UserSerializer
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+
+class UserViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        queryset = User.objects.all()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = User.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        queryset = User.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UserSerializer(user, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
