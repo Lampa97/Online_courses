e38fc1f2c02f34f773619966c44a0ade5f026f37
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-02-21 06:23:57 +0300

    added payments history for user

--- users/serializers.py
+++ users/serializers.py
@@ -3,13 +3,17 @@ from rest_framework import serializers
 from .models import User, Payment
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "email", "phone_number", "city"]
-
-
 class PaymentSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Payment
         fields = ["id", "user", "payment_date", "paid_course", "paid_lesson", "amount", "payment_method"]
+
+class UserSerializer(serializers.ModelSerializer):
+    payments = PaymentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "email", "phone_number", "city", "payments"]
+
+
