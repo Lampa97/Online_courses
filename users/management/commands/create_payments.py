9200cdde8f41f4ca1a226f3e5b02eb30cd2e4880
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-02-21 05:55:42 +0300

    changed logic of create_payments command

--- users/management/commands/create_payments.py
+++ users/management/commands/create_payments.py
@@ -1,11 +1,24 @@
 from django.core.management.base import BaseCommand
 from users.models import Payment, User
+from education.models import Course, Lesson
+import random
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+
         test_users = User.objects.filter(email__startswith="user_")
+        Payment.objects.filter(user__email__startswith="user_").delete()
+        python_course = Course.objects.get(title="Python")
+        django_course = Course.objects.get(title="Django")
+        flask_course = Course.objects.get(title="Flask")
+        courses_list = [python_course, django_course, flask_course]
+
+        python_lessons = Lesson.objects.filter(course=python_course)
+        django_lessons = Lesson.objects.filter(course=django_course)
+        flask_lessons = Lesson.objects.filter(course=flask_course)
+        lessons_list = [python_lessons, django_lessons, flask_lessons]
 
         for user in test_users:
-            Payment.objects.get_or_create(user=user, amount=100, payment_method="Cash")
-            Payment.objects.get_or_create(user=user, amount=2000, payment_method="Bank Transfer")
+            Payment.objects.get_or_create(user=user, amount=random.randint(100, 200), payment_method="Cash", paid_lesson=random.choice(random.choice(lessons_list)))
+            Payment.objects.get_or_create(user=user, amount=random.randint(1000, 2000), payment_method="Bank Transfer", paid_course=random.choice(courses_list))
         self.stdout.write(self.style.SUCCESS("Successfully created payments for test users"))
