3c8abd404468061cc24fc774a1790f99cc7fe720
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-02-21 04:48:45 +0300

    created Payment model

--- education/serializers.py
+++ education/serializers.py
@@ -4,15 +4,16 @@ from .models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
-    lessons_count = serializers.SerializerMethodField()
+    lessons_count = serializers.SerializerMethodField(read_only=True)
 
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
 
     class Meta:
         model = Course
         fields = ["title", "description", "lessons_count",]
 
-    def get_lessons_count(self, obj):
-        return obj.lessons.count()
+
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from education.models import Course, Lesson
 
 
 class User(AbstractUser):
@@ -23,3 +24,18 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Payment(models.Model):
+
+    PAYMENT_METHOD_CHOICES = [
+        ("Cash", "Cash"),
+        ("Bank Transfer", "Bank Transfer"),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="payments", verbose_name="Payment")
+    payment_date = models.DateTimeField(auto_now_add=True, verbose_name="Payment date")
+    paid_course = models.ForeignKey(Course, on_delete=models.CASCADE, blank=True, null=True, related_name="payments", verbose_name="Paid course")
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, blank=True, null=True, related_name="payments", verbose_name="Paid lesson")
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Amount")
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD_CHOICES, verbose_name="Payment method")
