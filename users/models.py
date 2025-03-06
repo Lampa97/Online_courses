838b796bede081375db804ce5cc88e32830cb395
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-03-06 17:59:46 +0300

    added integration with stripe

--- users/models.py
+++ users/models.py
@@ -34,7 +34,7 @@ class Payment(models.Model):
         ("Transfer", "Transfer"),
     ]
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="payments", verbose_name="Payment")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="payments", verbose_name="Payment")
     payment_date = models.DateTimeField(auto_now_add=True, verbose_name="Payment date")
     paid_course = models.ForeignKey(
         Course, on_delete=models.CASCADE, blank=True, null=True, related_name="payments", verbose_name="Paid course"
@@ -44,6 +44,9 @@ class Payment(models.Model):
     )
     amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Amount")
     payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD_CHOICES, verbose_name="Payment method")
+    session_id = models.CharField(max_length=255, blank=True, null=True, verbose_name="Session ID")
+    link = models.URLField(max_length=400, blank=True, null=True, verbose_name="Link")
+
 
     class Meta:
         verbose_name = "Payment"
